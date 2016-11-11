03d5c020d030695abfdf16d7558ac9882876080c
Don Kingery <kingery1@suddenlink.net>
2016-11-11 10:56:30 -0500

    some array practice

---
+++ e_ruby_cq.rb
@@ -0,0 +1,27 @@
+# some array practice
+don_1 = ["bread", "wine", "cheese", "thee", "basket", "blanket"]
+p don_1
+don_2 = [1,2,3]
+p don_2
+p don_1.length
+# p don_1.reverse
+# p don_1.uppcase
+# p don_1.capitalize
+p don_1[0]
+p don_1[0..2]
+p don_1
+don_1 << "wine glass"
+p don_1 
+if don_1.length <= 4
+    puts "Alert: less than 4 items"    
+    else
+        puts "Alert: more than 4 items" 
+end
+ 
+ counter = 0
+ while counter < 3
+     puts "Went through the loop"
+     counter = counter + 1
+end
+
+
